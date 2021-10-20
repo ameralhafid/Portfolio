5ec5066aa8aad0e99d919f28ca463d908ab67f22
ameralhafid <amer.alhafid95@gmail.com>
2021-10-20 15:36:06 +0200

    without conatct ready

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,24 +1,25 @@
 import React from "react";
 import Typed from "react-typed";
+import { Link } from "react-scroll";
 
 
 const Header = () => {
-    return (
-        <div className="header-wraper"> 
-          <div  className="main-info">
-          <h1>Web development and webshops promotions</h1>
-          <Typed 
-           className="typed-text"
-           strings={ ["Web development","Webshop Design","Facbook Ads","Google Ads","Instagram Ads"]}
-           typeSpeed={40}
-           backSpeed={60}
-           loop
-                 />
-           <a href="#" className="btn-main-offer" >contact me</a>      
-           </div> 
-        </div>
-    )
+  return (
+    <div id="home" className="header-wraper">
+      <div className="main-info">
+        <canvas></canvas>
+        <h1>web development and webShops promotions</h1>
+        <Typed
+          className="typed-text"
+          strings={["WebShop Design", "Web Development", "Facebook Ads", "Google Ads", "Instagram Ads"]}
+          typeSpeed={40}
+          backSpeed={60}
+          loop
+        />
+        <Link smooth={true} to="contacts" offset={-110} className="btn-main-offer">contact me</Link>
+      </div>
+    </div>
+  )
 }
 
-
-export default Header
+export default Header;
