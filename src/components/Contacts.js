af22594d0cc2b6671a09903e1649018598bf7853
ameralhafid <amer.alhafid95@gmail.com>
2021-10-22 01:02:52 +0200

    complete with edition from adili & favicon

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -37,7 +37,7 @@ const Contacts = () => {
     <div id="contacts" className="contacts">
       <div className="text-center">
         <h1>contact me</h1>
-        <p>Please fill out the form and describe you project needs and I'll contact you as soon as possible.</p>
+        <p>Please fill out the form and describe your project needs and I'll contact you at the earliest opportunity.</p>
         <span className="success-message">{successMessage}</span>
       </div>
       <div className="container">
